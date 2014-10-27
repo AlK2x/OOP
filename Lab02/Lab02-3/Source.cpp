b3c92526bdd4e9b9c8eda029901198fb88260204
AlK2x <AlexandrK86@gmail.com>
2014-10-27 23:14:52 +0300

    Добавлено перемножение матриц

--- Lab02/Lab02-3/Source.cpp
+++ Lab02/Lab02-3/Source.cpp
@@ -7,27 +7,30 @@ using namespace std;
 
 int main(int argc, char * argv[])
 {
-	if (argc < 2)
+	if (argc < 3)
 	{
 		cout << "Usage: invert.exe <matrix file>\n";
 		return 1;
 	}
 
-	string filename = argv[1];
-	ifstream ifs(filename);
-	if (ifs.is_open())
+	string filename1 = argv[1];
+	string filename2 = argv[2];
+	ifstream ifs1(filename1);
+	ifstream ifs2(filename2);
+	if (ifs1.is_open() && ifs2.is_open())
 	{
-
+		Matrix3x3 firstMatrix, secondMatrix;
+		firstMatrix.ReadFromFile(ifs1);
+		secondMatrix.ReadFromFile(ifs2);
+		cout << firstMatrix * secondMatrix << "\n";
 	}
 	else
 	{
-		cout << "Unable to open file: " << filename << "\n";
+		cout << "Unable to open file: " << filename1 << "\n";
 		return 1;
 	}
 
-	Matrix3x3 matrix;
-	matrix.ReadFromFile(ifs);
-	cout << matrix;
+	
 
 	return 0;
 }
--- Lab02/Lab02-3/matrix.h
+++ Lab02/Lab02-3/matrix.h
@@ -19,16 +19,19 @@ struct Matrix3x3
 
 	friend Matrix3x3& operator*(Matrix3x3 const& m1, Matrix3x3 const& m2)
 	{
+		Matrix3x3 * result = new Matrix3x3();
 		for (int i = 0; i < 3; ++i)
 		{
 			for (int j = 0; j < 3; ++j)
 			{
 				for (int k = 0; k < 3; ++k)
 				{
-
+					result->matrix[i][j] += m1.matrix[i][k] * m2.matrix[k][j];
 				}
 			}
 		}
+
+		return *result;
 	}
 
 	friend std::ostream& operator<< (std::ostream& os, Matrix3x3 const& m)
