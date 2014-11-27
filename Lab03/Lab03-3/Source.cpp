bdf0d829881e7651811d9fd85c0fdb1ff22724bc
AlK2x <AlexandrK86@gmail.com>
2014-11-27 23:50:21 +0300

    lab 3

---
+++ Lab03/Lab03-3/Source.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <map>
+#include <string>
+#include <algorithm>
+#include <iomanip>
+#include <cassert>
+
+using namespace std;
+
+map<string, int> ReadStringsFromStream(istream& ist)
+{
+	map<string, int> wordsFriquencyMap;
+	string inputString;
+	while (ist >> inputString)
+	{
+		transform(inputString.begin(), inputString.end(), inputString.begin(), ::tolower);
+		++wordsFriquencyMap[inputString];
+	}
+
+	return wordsFriquencyMap;
+}
+
+void PrintWordFriquency(map<string, int> wordsMap)
+{
+	for (auto const& wordPair : wordsMap)
+	{
+		cout << wordPair.first << "->" << wordPair.second << endl;
+	}
+}
+
+
+int main(int argc, char * argv[])
+{
+	map<string, int> wordsMap = ReadStringsFromStream(cin);
+
+	PrintWordFriquency(wordsMap);
+	return 0;
+}
